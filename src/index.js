4350aff0b73dd24b6dbe7e393313f8d02f48044c
Akhil Rajan <rajanakhil07@gmail.com>
2022-12-31 19:46:44 +0530

    first commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+
+const setupAndStartServer = async () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use("/api", apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`server started at port ${PORT}`);
+    });
+}
+
+setupAndStartServer();
